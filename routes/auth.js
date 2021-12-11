354ee39f3068137ec6aa5de3f62be5a49d1b1ea1
hape <hapemartini@gmail.com>
2021-12-11 17:56:14 +0100

    add registration method

--- routes/auth.js
+++ routes/auth.js
@@ -1,11 +1,16 @@
 const router = require('express').Router();
 const User = require('../models/User')
+const CryptoJS = require('crypto-js')
 
+// register
 router.post('/register', async (req, res) => {
     const newUser = new User({
         userName: req.body.userName,
         eMail: req.body.eMail,
-        password: req.body.password,
+        password: CryptoJS.AES.encrypt(
+            req.body.password,
+            process.env.PASSWORD_SECRET
+        ).toString(),
     });
 
     try {
