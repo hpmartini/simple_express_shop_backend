72b888b0dad76ffa2157ec1643d44a72b32e0eb8
hape <hapemartini@gmail.com>
2021-12-13 12:47:22 +0100

    add endpoints for products

--- routes/product.js
+++ routes/product.js
@@ -1,6 +1,7 @@
 const router = require('express').Router();
 const Product = require('../models/Product')
-const {verifyAdminRole} = require("./verifyToken");
+const {verifyAdminRole, verifyUser} = require("./verifyToken");
+const User = require("../models/User");
 
 router.post('/', verifyAdminRole, async (req, res) => {
     const newProduct = new Product(req.body);
@@ -13,39 +14,62 @@ router.post('/', verifyAdminRole, async (req, res) => {
     }
 });
 
-// // UPDATE
-// router.put('/:id', verifyUser, async (req, res) => {
-//     if (req.body.password) {
-//         req.body.password = CryptoJS.AES.encrypt(
-//             req.body.password,
-//             process.env.PASSWORD_SECRET
-//         ).toString();
-//     }
-//
-//     try {
-//         const updatedUser = await User.findByIdAndUpdate(req.params.id, {
-//             $set: req.body
-//         }, {new:true})
-//         res.status(200).json(updatedUser);
-//     } catch (error) {
-//         res.status(500).json(error);
-//     }
-// })
-//
-// // GET ALL USER
-// const {verifyAdminRole, verifyUser} = require("./verifyToken");
-// const User = require("../models/User");
-// const CryptoJS = require("crypto-js");
-// router.get('/', verifyAdminRole, async (req, res) => {
-//     const query = req.query.new;
-//     try {
-//         const users = query
-//             ? await User.find().sort({ _id: -1 }).limit(5)
-//             : await User.find()
-//         res.status(200).json(users);
-//     } catch (error) {
-//         res.status(500).json(error);
-//     }
-// })
+// UPDATE
+router.put('/:id', verifyAdminRole, async (req, res) => {
+    try {
+        const updatedProduct = await Product.findByIdAndUpdate(req.params.id, {
+            $set: req.body
+        }, {new: true})
+        res.status(200).json(updatedProduct);
+    } catch (error) {
+        res.status(500).json(error);
+    }
+})
+
+// DELETE
+router.delete('/:id', verifyAdminRole, async (req, res) => {
+    try {
+        await Product.findByIdAndDelete(req.params.id);
+        res.status(200).json('Product has been deleted');
+    } catch (error) {
+        res.status(500).json(error);
+    }
+})
+
+// GET
+router.get('/find/:id', async (req, res) => {
+    try {
+        const product = await Product.findById(req.params.id);
+        res.status(200).json(product);
+    } catch (error) {
+        res.status(500).json(error);
+    }
+})
+
+// GET ALL PRODUCT
+router.get('/', async (req, res) => {
+    const qNew = req.query.new;
+    const qCetegory = req.query.category;
+
+    try {
+        let products;
+
+        if (qNew) {
+            products = await Product.find().sort({createdAt: -1}).limit(5);
+        } else if (qCetegory) {
+            products = await Product.find({
+                categories: {
+                    $in: [qCetegory],
+                }
+            })
+        } else {
+            products = await Product.find();
+        }
+
+        res.status(200).json(products);
+    } catch (error) {
+        res.status(500).json(error);
+    }
+})
 
 module.exports = router;
