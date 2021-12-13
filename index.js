ba42f32b8164ab6d09bd2a9fbcdd42905763cd4f
hape <hapemartini@gmail.com>
2021-12-13 13:42:06 +0100

    integrate stripe payments

--- index.js
+++ index.js
@@ -4,6 +4,10 @@ const dotenv = require('dotenv');
 const userRoutes = require('./routes/user');
 const authRoutes = require('./routes/auth');
 const productRoutes = require('./routes/product');
+const cartRoute = require("./routes/cart");
+const orderRoute = require("./routes/order");
+const stripeRoute = require("./routes/stripe");
+const cors = require("cors");
 
 dotenv.config();
 
@@ -14,10 +18,14 @@ mongoose.connect(process.env.MONGO_URL)
     .then(() => console.log('Connection established'))
     .catch((error) => console.log(error));
 
+app.use(cors());
 app.use(express.json());
 app.use("/api/auth", authRoutes);
 app.use("/api/users", userRoutes);
 app.use("/api/products", productRoutes);
+app.use("/api/carts", cartRoute);
+app.use("/api/orders", orderRoute);
+app.use("/api/checkout", stripeRoute);
 
 app.listen(port, () => {
     console.log("Backend listening on port", port);
