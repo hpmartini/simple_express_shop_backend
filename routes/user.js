2e6c6240cb7f3ae45b819cba782c7c32b88b3181
hape <hapemartini@gmail.com>
2021-12-13 11:01:03 +0100

    add update user

--- routes/user.js
+++ routes/user.js
@@ -1,5 +1,25 @@
+const {verifyUser} = require("./verifyToken");
+const CryptoJS = require("crypto-js");
+const User = require('../models/User')
 const router = require('express').Router();
 
+// UPDATE
+router.put('/:id', verifyUser, async (req, res) => {
+    if (req.body.password) {
+        req.body.password = CryptoJS.AES.encrypt(
+            req.body.password,
+            process.env.PASSWORD_SECRET
+        ).toString();
+    }
 
+    try {
+        const updatedUser = await User.findByIdAndUpdate(req.params.id, {
+            $set: req.body
+        }, {new:true})
+        res.status(200).json(updatedUser);
+    } catch (error) {
+        res.status(500).json(error);
+    }
+})
 
 module.exports = router;
---
+++ routes/verifyToken.js
@@ -0,0 +1,39 @@
+const jwt = require('jsonwebtoken');
+
+const verifyToken = (req, res, next) => {
+    const authHeader = req.headers.authorization;
+    if (authHeader) {
+        const token = authHeader.split(' ')[1];
+        jwt.verify(token, process.env.JWT_SECRET, (err, user) => {
+            if (err) {
+                res.status(403).json('Token is invalid!');
+            }
+            req.user = user;
+            next();
+        });
+    } else {
+        return res.status(401).json('You\'re not authenticated');
+    }
+}
+
+const verifyUser = (req, res, next) => {
+    verifyToken(req, res, () => {
+        if (req.user.id === req.params.id || req.user.isAdmin) {
+            next();
+        } else {
+            res.status(403).json('Permission denied.')
+        }
+    })
+}
+
+const verifyAdminRole = (req, res, next) => {
+    verifyToken(req, res, () => {
+        if (req.user.isAdmin) {
+            next();
+        } else {
+            res.status(403).json('Permission denied.')
+        }
+    })
+}
+
+module.exports = {verifyToken, verifyAdminRole, verifyUser};
